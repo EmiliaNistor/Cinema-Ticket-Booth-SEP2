d4503926d722b72cdaf25c11b6f2e36cb1799051
Hugo Guiomar <82777546+HugoSilva4@users.noreply.github.com>
2023-07-20 20:41:11 +0200

    fixed

--- src/main/java/View/controllers/purchaseTicketController.java
+++ src/main/java/View/controllers/purchaseTicketController.java
@@ -1,4 +1,5 @@
 package View.controllers;
+
 import Util.ViewModelFactory;
 import ViewModel.purchaseTicketViewModel;
 import View.ViewHandler;
@@ -10,52 +11,49 @@ import javafx.scene.control.Label;
 public class purchaseTicketController {
 
 
+    @FXML
+    private Button purchaseTicket;
 
-        @FXML
-        private Button purchaseTicket;
+    @FXML
+    private Label movieName;
 
-        @FXML
-        private Label movieName;
+    @FXML
+    private Label movieScreen;
 
-        @FXML
-        private Label movieScreen;
+    @FXML
+    private Button movieHour1;
 
-        @FXML
-        private Button movieHour1;
+    @FXML
+    private Button movieHour2;
 
-        @FXML
-        private Button movieHour2;
+    @FXML
+    private Label moviePrice;
 
-        @FXML
-        private Label moviePrice;
+    private purchaseTicketViewModel viewModel;
+    private ViewHandler viewHandler;
 
-        private purchaseTicketViewModel viewModel;
-        private ViewHandler viewHandler;
+    public void init(ViewHandler viewHandler, ViewModelFactory vmf) {
 
-        public void init(ViewHandler viewHandler, ViewModelFactory vmf) {
+        this.viewHandler = viewHandler;
+        this.viewModel = vmf.getPurchaseTicketViewModel();
+        movieName.textProperty().bind(viewModel.movieNameProperty());
+        movieScreen.textProperty().bind(viewModel.movieScreenProperty());
+        movieHour1.textProperty().bind(viewModel.movieHour1Property());
+        movieHour2.textProperty().bind(viewModel.movieHour2Property());
+        moviePrice.textProperty().bind(viewModel.moviePriceProperty());
+        
+    }
 
-            this.viewHandler = viewHandler;
-            this.viewModel = vmf.getPurchaseTicketViewModel();
-            movieName.textProperty().bind(viewModel.movieNameProperty());
-            movieScreen.textProperty().bind(viewModel.movieScreenProperty());
-            movieHour1.textProperty().bind(viewModel.movieHour1Property());
-            movieHour2.textProperty().bind(viewModel.movieHour2Property());
-            moviePrice.textProperty().bind(viewModel.moviePriceProperty());
+    @FXML
+    private void purchaseTicket() {
 
+        String selectedMovieName = viewModel.getMovieName();
+        String selectedScreen = viewModel.getMovieScreen();
+        String selectedHour = "";
+        String ticketPrice = viewModel.getMoviePrice();
 
-        }
+        viewHandler.openTicketInformation();
+    }
 
-        @FXML
-        private void buyTicket() {
-            if {
-                String selectedMovieName = viewModel.getMovieName();
-                String selectedScreen = viewModel.getMovieScreen();
-                String selectedHour = "";
-                String ticketPrice = viewModel.getMoviePrice();
-            }else {
-                viewHandler.openTicketInformation();
-            }
-
-        }
 
 }

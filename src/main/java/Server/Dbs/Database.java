62ef6e1168874c3cf3e618aee8ceda69b824d6a3
Emilia Nistor <emilia.georgiana24@gmail.com>
2023-07-30 20:37:44 +0200

    add menu and get menu info db

--- src/main/java/Server/Dbs/Database.java
+++ src/main/java/Server/Dbs/Database.java
@@ -1,11 +1,18 @@
 package Server.Dbs;
 
+import Shared.Model.Menu;
 import Shared.Model.Movie;
 import Shared.Model.Seat;
 import Shared.Model.Ticket;
 
+import java.util.ArrayList;
+
 public interface Database {
+     int getSeatId(int screen, String row, int number);
      void makePurchase(Ticket ticket, Seat seat, Movie movie);
      Ticket getTicket(int ticketId);
      void cancelTicket(int ticketId);
+     Menu addMenu(String food, double price);
+     ArrayList<Menu> getMenu();
+
 }

7d11f61b8b7aea26154bd11db15bc5295a9c3aa8
Hugo Guiomar <82777546+HugoSilva4@users.noreply.github.com>
2023-08-08 16:15:34 +0200

    Unit_Tests

---
+++ src/test/java/Client/ViewModel/MainSceneViewModelTest.java
@@ -0,0 +1,71 @@
+package Client.ViewModel;
+
+import Client.Core.ModelFactory;
+import Client.Core.ViewModelFactory;
+import Client.Model.AccountModel;
+import Client.Model.IAccountModel;
+import Shared.Network.IRMIServer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainSceneViewModelTest {
+
+    private MainSceneViewModel viewModel;
+    private IRMIServer IRMIServer;
+    private ModelFactory ModelFactory;
+
+
+
+    @BeforeEach
+    void setUp() {
+        ViewModelFactory viewModelFactory = new ViewModelFactory(ModelFactory);
+        IAccountModel accountModel = new AccountModel(IRMIServer);
+
+        viewModel = new MainSceneViewModel(viewModelFactory, accountModel);
+    }
+
+    @Test
+    void testInitialState() {
+        assertFalse(viewModel.loggedInProperty().get());
+        assertFalse(viewModel.administratorProperty().get());
+        assertEquals("Log In", viewModel.logInTextStringProperty().get());
+        assertTrue(viewModel.movieListVisible().get());
+        assertFalse(viewModel.ticketInfoVisible().get());
+    }
+
+    //Combined test for Login and Logout
+    @Test
+    void testLogInAndOut() {
+        assertFalse(viewModel.isLoggedIn());
+
+        viewModel.loggedInProperty();
+        assertTrue(viewModel.loggedInProperty().get());
+        assertFalse(viewModel.administratorProperty().get());
+        assertEquals("Log Out", viewModel.logInTextStringProperty().get());
+
+        viewModel.logOut();
+        assertFalse(viewModel.loggedInProperty().get());
+        assertFalse(viewModel.administratorProperty().get());
+        assertEquals("Log In", viewModel.logInTextStringProperty().get());
+    }
+
+    //Combined Test for changing scenes
+    @Test
+    void testChangeToMovieListAndTicketInfo() {
+        assertTrue(viewModel.movieListVisible().get());
+        assertFalse(viewModel.ticketInfoVisible().get());
+
+        viewModel.changeToTicketInfo();
+        assertFalse(viewModel.movieListVisible().get());
+        assertTrue(viewModel.ticketInfoVisible().get());
+
+        viewModel.changeToMovieList();
+        assertTrue(viewModel.movieListVisible().get());
+        assertFalse(viewModel.ticketInfoVisible().get());
+    }
+    
+}
+
